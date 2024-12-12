2180c05601114bd078f3f84bf7cdeb4b7beb1511
Ali Yara <ali.yara.fr@gmail.com>
2024-12-12 23:26:38 +0100

    add gsap in team page

--- src/components/team/Team.jsx
+++ src/components/team/Team.jsx
@@ -1,22 +1,47 @@
+"use client";
 import React from "react";
+import { useEffect } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 import style from "@/styles/team/team.module.css";
 
 function Team() {
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    gsap.fromTo(
+      ".card_anim",
+      {
+        y: "10%",
+        opacity: 0,
+      },
+      {
+        y: 0,
+        opacity: 1,
+        duration: 0.5,
+        stagger: 0.3,
+        scrollTrigger: {
+          trigger: ".card_anim",
+          start: "top bottom",
+          end: "bottom center",
+        },
+      }
+    );
+  }, []);
   const teamMember = [
     {
       pic: "/images/team/kamal.jpg",
       name: "Kamal Sekkar",
-      position: "Senior Photographer",
+      position: "Creative Director, Founder",
     },
     {
       pic: "/images/team/omflow.jpg",
       name: "Oussama Mounaji",
-      position: "Chief Operations Officer",
+      position: "Chief Operations Officer, Founder",
     },
     {
       pic: "/images/team/moustafa.jpg",
       name: "Elmostapha Aamre",
-      position: "Chief Financial Officer",
+      position: "Chief Executive Officer, Founder",
     },
     {
       pic: "/images/team/guarouani.jpg",
@@ -56,7 +81,7 @@ function Team() {
     {
       pic: "/images/team/abdalhadi.jpg",
       name: "Abdelhadi Bouchrouaat",
-      position: "Branding & Visual Identity Designer",
+      position: "Visual Identity Designer",
     },
   ];
   return (
@@ -70,7 +95,7 @@ function Team() {
         <div className={style.cross_line}></div>
         <div className={style.cards_list}>
           {teamMember.map((member, index) => (
-            <div className={style.card} key={index}>
+            <div className={`${style.card} card_anim`} key={index}>
               <div className={style.picture}>
                 <img src={member.pic} alt={member.name} loading="lazy" />
               </div>
