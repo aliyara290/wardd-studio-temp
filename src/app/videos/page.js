2a6549c7b2349cf998e51ebb41f40881eba17109
Mohamed sekkar <mohamedsekkar1110@gmail.com>
2025-05-24 15:23:29 +0100

    update videos component

--- src/app/videos/page.js
+++ src/app/videos/page.js
@@ -1,4 +1,4 @@
-import VideoShowcase from "../../components/video/VideoShowcase";
+import VideoShowcase from "@/components/video/VideoShowcase";
 import Header from "@/components/home/Header";
 import Contact from "@/components/contact/Contact";
 import Footer from "@/components/home/Footer";
